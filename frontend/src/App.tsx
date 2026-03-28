0c58e85511c114240ba84dd77fcddfeef8751ed0
raes2771-creator <rae.s2771@gmail.com>
2026-03-28 14:37:37 +0000

    chanegs to front end

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,12 +9,59 @@ import {
   RadioGroup,
   Skeleton,
   VStack,
+  Card,
+  Field,
+  Textarea
 } from "@chakra-ui/react"
 
+const pngImages = import.meta.glob('./assets/*.png', { eager: true, import: 'default' })
+
 export default function Page() {
+  const backgroundImage = pngImages['./assets/Background day 1 - 15.png'];
+
   return (
-    <Box textAlign="center" fontSize="xl" pt="30vh">
-      <Heading>Gratitude 100!</Heading>
-    </Box>
+    <Box 
+        alignItems="center" 
+        fontSize="xl" 
+        minH ="100vh" 
+        display="flex" 
+        justifyContent="flex-start"
+        px="25px"
+        py="25px"
+        bgImage={backgroundImage ? `url(${backgroundImage})` : undefined}
+        bgSize="cover"
+        bgRepeat="no-repeat"
+        bgPosition="center"
+       > 
+      <Card.Root
+        borderRadius="md"
+        bg="whiteAlpha.800"
+        textAlign="center"
+        width="50%"
+        height="100vh"
+        color="black"
+        fontFamily="'Patrick Hand', cursive"
+        sx={{
+          "@media (orientation: portrait)": {
+            height: "100vh",
+            width: "100%"
+          }
+        }}
+      > 
+        <Card.Header> Temp text for before backend connection </Card.Header>
+        <Field.Root 
+          display="flex"
+          flexDirection="column"
+          alignItems="center"> 
+          <Field.Label> Write your reflections here! </Field.Label>
+          <Textarea 
+            placeholder="Write your reflections here!"
+            size="lg"
+            height="200px"
+            width="80%"
+          />
+        </Field.Root>
+        </Card.Root>
+      </Box>
   )
 }
